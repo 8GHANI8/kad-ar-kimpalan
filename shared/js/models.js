bacb5acfcac0d9714a0cbb79f14517abc1749ceb
GHANI <g-47244292@moe-dl.edu.my>
2026-08-24 13:03:05 +0800

    Update models.js

--- shared/js/models.js
+++ shared/js/models.js
@@ -213,4 +213,4 @@ function registerGLBModel(name, path, scale){
 }
 
 // Contoh (padam "//" di depan bila kamu betul-betul ada fail .glb untuk item ini):
-registerGLBModel("buildSMAWMachine", "assets/models/pemegang-elektrod.glb", 0.4);
+registerGLBModel("buildSMAWMachine", "shared/assets/models/pemegang-elektrod.glb", 0.4);
