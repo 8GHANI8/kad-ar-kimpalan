28a37d052ab19a8b77054a83850de088456692ea
GHANI <g-47244292@moe-dl.edu.my>
2026-09-02 10:26:51 +0800

    Add new GLB models for GroundClamp and WeldingCable

--- shared/js/models.js
+++ shared/js/models.js
@@ -223,5 +223,6 @@ function registerGLBModel(name, path, scale){
 // Contoh (padam "//" di depan bila kamu betul-betul ada fail .glb untuk item ini):
 // registerGLBModel("buildSMAWMachine", "assets/models/mesin-smaw.glb", 0.4);
 registerGLBModel("buildElectrodeHolder", "assets/models/electrode-holde.glb", 0.4);
-
+registerGLBModel("GroundClamp", "assets/models/bumi.glb", 0.4);
+registerGLBModel("WeldingCable", "assets/models/kabel.glb", 0.4);
 
